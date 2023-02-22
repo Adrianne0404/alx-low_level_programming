c62d6ae1e250a9ba7df9b7a7ac9316f57258f6c7
adrianne <adrianne.williams007@gmail.com>
2023-02-22 15:17:22 -0800

    8 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,21 +2,17 @@
 
 
 /**
- *jack_bauer - prints every minute of the day of 
- *             Jack Bauer, starting from 00:00 to 23:59
+ *jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
-        int hour, minute;
-
-
-	for (hour = 0; hour <= 23; hour++)
-        {
+	int hour, minute;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-	        for (minute = 0; minute <= 59; minute++)
-		whil
+		for (minute = 0; minute <= 59; minute++)
+		{
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
