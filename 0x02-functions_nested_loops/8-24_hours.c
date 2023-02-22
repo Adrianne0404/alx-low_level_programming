4524f89df00210092ddaadfa2cae2493f49f1c48
adrianne <adrianne.williams007@gmail.com>
2023-02-22 14:53:46 -0800

    8 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ *jack_bauer - prints every minute of the day of Jack Bauer
+ *starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
@@ -23,6 +23,7 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
+			urs.c8-24_hours.cjdhbfhwvfjh
 			_putchar('\n');
 			j++;
 		}
