090d6ff18ac188610ce432eb5f1e5799a7bc9e29
adrianne <adrianne.williams007@gmail.com>
2023-02-24 15:48:38 -0800

    3print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_numbers - checks for a digit (0 through to 9).
+ *
+ * Return: Always (0) Success.
+ */
+void print_ numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
