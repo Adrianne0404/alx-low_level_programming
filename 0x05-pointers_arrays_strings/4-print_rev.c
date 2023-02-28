046da2cf67cc25c26703e143cec7c11f13d1f256
adrianne <adrianne.williams007@gmail.com>
2023-02-27 19:59:27 -0800

    4 print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include "holberton.h"
+
 
 /**
  * print_rev - this function prints a reverse string
@@ -10,6 +11,8 @@ void print_rev(char *s)
 	int index;
 
 	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index -1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
 	}
