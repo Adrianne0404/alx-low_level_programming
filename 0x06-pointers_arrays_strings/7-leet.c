1082d036646ade8aeda9f6d046c13f196a6b9922
adrianne <adrianne.williams007@gmail.com>
2023-03-03 15:06:41 -0800

    7 leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,24 @@
 #include "main.h"
 
 /**
- * cap_string - take strings and capitalize words
+ * leet - take strings and capitalize words
  * @a: string to capitalize
  * Return: capitalized words
  */
 
-char *cap_string(char *a)
+char *leet(char *a)
 {
 	int i, x;
-	char *seperators = ",;.!?\"(){} \n\t";
+	char check[] = "aAeEoOtTlL";
+	char change[] = "4433007711";
 
 	for (i = 0; *(a + i) != '\0'; i++)
 	{
-		if (*(a + i) >= 'a' && *(a + i) <= 'z')
-			for (x = 0; *(seperators + x) != '\0'; x++)
+		for (x = 0; check[x]; x++)
+			if (check[x] == *(a + i))
 			{
-				if (*(a + i - 1) == *(seperators + x))
-				{
-					*(a + i) -= 32;
-					break;
-				}
-				else if (i == 0)
-				{
-					*(a + i) -= 32;
-					break;
-				}
+				*(a + i) = change[x];
+				break;
 			}
 	}
 	return (a);
