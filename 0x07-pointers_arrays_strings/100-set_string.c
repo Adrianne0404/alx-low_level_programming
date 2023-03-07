9e4618c275ebea34d3d1b7f5c383eb1f0b967862
adrianne <adrianne.williams007@gmail.com>
2023-03-07 14:19:06 -0800

    100 set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,20 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_diagsums -  prints sums
- * @a: a pointer
- * @size: size
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
  */
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int i, sum1 = 0, sum2 = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		sum1 += *(a + (size * i + i));
-		sum2 += *(a + (size * i + size - 1 - i));
-	}
-	printf("%d, ", sum1);
-	printf("%d\n", sum2);
+	*s = to;
 }
